13ddff15916ec7c9d2774af538d921ca3eafafa0
nullptr <2281525307@qq.com>
2020-10-06 20:03:55 +0800

    项目重构

---
+++ CommonUtil/CommonUtil.cpp
@@ -0,0 +1,160 @@
+#include "pch.h"
+#include "CommonUtil.h"
+
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <numeric>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+string algorithm::utf8StrToAsciiStr(const char* utf8Str)
+{
+	wstring unicodeStr = utf8StrToUnicodeStr(utf8Str);
+	int strLen = ::WideCharToMultiByte(CP_ACP, 0, unicodeStr.c_str(), unicodeStr.size(), NULL, 0, NULL, NULL);
+	char* str = new char[strLen + 1];
+	::WideCharToMultiByte(CP_ACP, 0, unicodeStr.c_str(), unicodeStr.size(), str, (int)strlen(str), NULL, NULL);
+	string asciiStr(str);
+	// 删除分配的存储空间
+	delete[] str;
+	// 将指针所指向地址置为空
+	str = nullptr;
+	// 返回编码后的字符串
+	return asciiStr;
+}
+
+std::wstring algorithm::utf8StrToUnicodeStr(const char * utf8Str)
+{
+	// 预转换，计算所需空间的大小;
+	int wstrLen = ::MultiByteToWideChar(CP_UTF8, NULL, utf8Str, (int)strlen(utf8Str), NULL, 0);
+	// 动态分配字符串存储空间，需要将计算得到的空间大小加1，用于存储 ‘\0’
+	wchar_t* wstr = new wchar_t[wstrLen + 1];
+	// 转换字符串编码
+	::MultiByteToWideChar(CP_UTF8, NULL, utf8Str, (int)strlen(utf8Str), wstr, wstrLen);
+	// 添加字符串结束符
+	wstr[wstrLen] = '\0';
+	// 获取编码后的字符串
+	wstring unicodeStr(wstr);
+	// 删除分配的存储空间
+	delete[] wstr;
+	// 将指针所指向地址置为空
+	wstr = nullptr;
+	return unicodeStr;
+}
+
+std::string algorithm::getNowDateStr()
+{
+	auto time = system_clock::to_time_t(system_clock::now());
+
+	//转为字符串
+	stringstream stream;
+	string timeStr;
+	stream << put_time(localtime(&time), "%Y-%m-%d");
+	stream >> timeStr;
+
+	return timeStr;
+}
+
+string algorithm::getNowTimeStr()
+{
+	auto time = system_clock::to_time_t(system_clock::now());
+
+	//转为字符串
+	stringstream stream;
+	string timeStr;
+	stream << put_time(localtime(&time), "%Y-%m-%d-%H:%M:%S");
+	stream >> timeStr;
+
+	return timeStr;
+}
+
+string algorithm::parseDateTimeToString(const int& date, const int& time)
+{
+	return parseDateToString(date) + "-" + parseTimeToString(time);
+}
+
+std::string algorithm::parseDateToString(const int & date)
+{
+	stringstream sstream;
+	string dateStr;
+
+	// 写入时间
+	sstream << date;
+	sstream >> dateStr;
+	sstream.clear();
+
+	sstream << dateStr.substr(0, 4) << "-";
+	sstream << dateStr.substr(4, 2) << "-";
+	sstream << dateStr.substr(6, 2);
+	
+	dateStr.clear();
+	sstream >> dateStr;
+	sstream.clear();
+
+	return dateStr;
+}
+
+string algorithm::parseTimeToString(const int& time)
+{
+	stringstream sstream;
+	string timeStr;
+
+	sstream << time;
+	sstream >> timeStr;
+	sstream.clear();
+
+	// 判断小时部分是否为两位数，如10点则为10，9点则为9
+	if (timeStr.size() == 9) {
+		// 获取小时部分
+		sstream << timeStr.substr(0, 2) << ":";
+		// 获取分钟部分
+		sstream << timeStr.substr(2, 2) << ":";
+		// 获取秒部分
+		sstream << timeStr.substr(4, 2) << ":";
+		// 获取秒部分
+		sstream << timeStr.substr(6, 3);
+	}
+	else {
+		sstream << timeStr.substr(0, 1) << ":";
+		sstream << timeStr.substr(1, 2) << ":";
+		sstream << timeStr.substr(3, 2) << ":";
+		sstream << timeStr.substr(5, 3);
+	}
+
+	timeStr.clear();
+	sstream >> timeStr;
+	sstream.clear();
+
+	return timeStr;
+}
+
+int algorithm::subTime(int time, int seconds)
+{
+	// 减去当前时间的毫秒值
+	time -= time % 1000;
+	// 将要减去的描述转换为毫秒单位
+	seconds *= 1000;
+
+	// 计算当前时间的分钟值部分
+	int minute = time / 100000 % 100;
+	// 判断是否为整点
+	if (minute == 0) {
+		// 减去59分钟的值
+		return time - (40 + 1) * 100000;
+	}
+	// 减去一分钟的值
+	return time - 100000;
+}
+
+string algorithm::getRelativePath(const string & filePath)
+{
+	size_t splitPos = filePath.find_last_of('\\');
+	return filePath.substr(splitPos + 1);
+}
+
+double algorithm::percentageConvert(const double & percentage)
+{
+	return percentage / 100;
+}
---
+++ CommonUtil/CommonUtil.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+
+#include "pch.h"
+#include <string>
+
+/**
+ * 常用算法函数
+ *
+ * @author nullptr
+ * @version 1.1 2020-8-16 增加获取文件相对路径算法和百分比转化函数
+ * @version 1.0 2020-7-13
+ * @since 1.0 2020-7-13
+ */
+namespace algorithm {
+	/**
+	 *	@brief UTF-8编码字符串转换成ascii编码字符串
+	 *
+	 * @param utf8Str utf8编码字符串
+	 * @return 转换后的ascii编码字符串
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::string utf8StrToAsciiStr(const char* utf8Str);
+		
+	/**
+	 *	@brief UTF-8编码字符串转换成unicode编码字符串
+	 *
+	 * @param utf8Str utf8编码字符串
+	 * @return 转换后的unicode编码字符串
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::wstring utf8StrToUnicodeStr(const char* utf8Str);
+	
+	/**
+	 * @brief 获取当前系统日期，并以字符串形式返回
+	 *
+	 * @return 当前时间的字符串表示
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::string getNowDateStr();
+
+	/**
+	 * @brief 获取当前系统时间，并以字符串形式返回
+	 * 
+	 * @return 当前时间的字符串表示
+	 * @since 1.0 
+	 */
+	__declspec(dllexport) std::string getNowTimeStr();
+
+	/**
+	 * @brief 解析日期整数和时间整数表示为字符串
+	 * 
+	 * @param date 日期整数表示(如：20200320—— 表示为 2020-03-20)
+	 * @param time 时间整数表示(如：30900000—— 表示为 3:09:00:000)
+	 * @return 解析后的时间字符串
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::string parseDateTimeToString(const int& date, const int& time);
+	
+	/**
+	 * @brief 解析日期整数表示为字符串
+	 *
+	 * @param date 日期整数表示(如：20200320—— 表示为 2020-03-20)
+	 * @return 解析后的时间字符串
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::string parseDateToString(const int& date);
+
+	/**
+	 * @brief 解析时间整数表示为字符串
+	 *
+	 * @param time 时间整数表示(如：30900000—— 表示为 3:09:00:000)
+	 * @return 解析后的时间字符串
+	 * @since 1.0
+	 */
+	__declspec(dllexport) std::string parseTimeToString(const int& time);
+
+	/**
+	 * @brief 时间整数减法运算
+	 * 
+	 * @param time 时间整数表示(如：30900000—— 表示为 3:09:00:000)
+	 * @param seconds 需要减去的秒数
+	 * @return 计算得到的新时间整数表示
+	 * @since 1.0
+	 */
+	__declspec(dllexport) int subTime(int time, int seconds);
+
+	/**
+	 * @brief 获取相对文件路径
+	 *
+	 * @param absolutePath 绝对文件路径
+	 * @return 相对文件路径
+	 * @since 1.1
+	 */
+	__declspec(dllexport) std::string getRelativePath(const std::string& absolutePath);
+	
+	/**
+	 * @brief 百分比转化
+	 *
+	 * @param percentage 百分比小数表示，如3%=0.03， 30%=0.3，30.33%=0.333
+	 * @return 百分比的小数表示
+	 * @since 1.1
+	 */
+	__declspec(dllexport) double percentageConvert(const double& percentage);
+
+	//__declspec(dllexport) std::string formatStr(const char* format, ...);
+}
